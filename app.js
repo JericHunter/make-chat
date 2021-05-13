0ee7729b83e969f474242d8bf72a3c3a151bf383
JericHunter <jeric.hunterr@gmail.com>
2021-05-13 02:19:06 -0700

    user can log in

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-//App.js
-const express = require('express');
+//app.js
+const express = require("express");
 const app = express();
+const server = require("http").Server(app);
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
-const server = require('http').Server(app);
-
+//Establish your public folder
+app.use("/public", express.static("public"));
 
 //Socket.io
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
+let onlineUsers = {};
+//Save the channels in this object.
+let channels = { General: [] };
 io.on("connection", (socket) => {
-  console.log("🔌 New user connected! 🔌");
-})
-//Express View Engine for Handlebars
-const exphbs  = require('express-handlebars');
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-//Establish your public folder
-app.use('/public', express.static('public'))
+	// This file will be read on new socket connections
+	require("./sockets/chat.js")(io, socket, onlineUsers, channels);
+	console.log("🔌 New user connected! 🔌");
+});
 
-server.listen('3000', () => {
-  console.log('Server listening on Port 3000');
+app.get("/", (req, res) => {
+	res.render("layouts/main.handlebars");
+});
+
+server.listen("3000", () => {
+	console.log("Server listening on Port 3000");
+});
